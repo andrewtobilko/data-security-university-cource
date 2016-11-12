85167566c512bb4fedf7acca76a9bed45ad368c9
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-11-12 23:53:01 +0200

    lab 5 [3rd, template]

--- src/com/tobilko/lab5/EnhancedCaesarCipher.java
+++ src/com/tobilko/lab5/EnhancedCaesarCipher.java
@@ -1,7 +1,36 @@
 package com.tobilko.lab5;
 
+import org.apache.commons.collections4.BidiMap;
+
+import java.io.IOException;
+
 /**
+ *
  * Created by Andrew Tobilko on 11/10/2016.
+ *
  */
-public class EnhancedCaesarCipher {
+public class EnhancedCaesarCipher extends CaesarCipher {
+
+    private BidiMap<Character, Integer> map;
+
+    public EnhancedCaesarCipher(String fileName, int key) {
+        super(key);
+        try {
+            map = CaesarUtils.getCustomAlphabetFromFile(fileName);
+            super.size = map.size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String encode(String toEncode) {
+        return null;
+    }
+
+    @Override
+    public String decode(String toDecode) {
+        return null;
+    }
+
 }
